46a06ba5ee1a77c0c64885f065d18b0b0f2063cf
mironalex00 <60828762+mironalex00@users.noreply.github.com>
2022-06-21 20:30:55 +0200

    Hotfix FirewallController

--- src/controllers/FirewallController.js
+++ src/controllers/FirewallController.js
@@ -12,49 +12,31 @@ function interfaces(req, res){
           res.redirect('/auth/logout');
         }else if(req.session.loggedin !== true) {
           res.redirect('/auth/login');
-        }else{            
-            const interface = Object.entries(getInterfaces()).find(([k, v]) => { return v.id === parseInt(req.params.id)});
+        }else{    
+            const interface = getInterfaces().find(x => x.interfaceId === parseInt(req.params.id));
             if(interface === undefined){
                 res.status(404).render('errors/404', { layout: 'root' , name: req.session.name,});
             }else{
-                const settings = interface[interface.length - 1];
+                const settings = interface.settings;
                 conn.query('SELECT * FROM interfaces', (err, rows) => {
                     if(err){
                         logger.error(err.message);
-                        res.status(500).send('Error desconocido');
+                        res.status(500).send('Erlror desconocido');
                     }else{
-                        const result = JSON.parse(Object.values(JSON.parse(JSON.stringify(rows))).find(() => true).settingsComponent);
-                        const newRes = result.find(x => x.interfaceId === interface[1].id);
-                        if(newRes === undefined || typeof newRes === 'undefined'){
-                            result.push({'interfaceId': interface[1].id, 'settings': { isEnabled: true, ip_setting: 'dhcp', isLocked: true, desc: `Interfaz ${interface[0]}`}})
-                            conn.query('UPDATE configuration SET valConfiguration = ? WHERE idConfiguration = (SELECT idConfiguration WHERE idComponent = (SELECT nameComponent FROM interfaces))', [JSON.stringify(result)], (err, rows) => {
-                                if(err || rows.affectedRows !== 1){
-                                    if(err) logger.error(err.message);
-                                    res.redirect('/');
-                                }else{
-                                    logger.warn(`Añadida interfaz (${interface[0]}). Fecha ${new Date().toJSON()}`)
-                                    interfaces(req, res);
-                                }
-                            });
-                        }else{
-                            const final = Object.assign(settings, newRes.settings);
-                            res.status(200).render('interface', { 
-                                title: 'ARMwall - Interfaces', 
-                                layout: 'root', 
-                                name: req.session.name, 
-                                interface: interface[0],
-                                settings: final,
-                                section: {name: 'Cortafuegos', pointer: `interfaz ${interface[1].id}`} 
-                            });
-                        }
+                        const result = JSON.parse(Object.values(JSON.parse(JSON.stringify(rows))).find(() => true).settingsComponent).find(x => x.interfaceId === interface.interfaceId);
+                        
                     }
                 });
             }
         }
     });
 }
+function rules(req, res){
+
+}
 
 module.exports = {
     main,
-    interfaces
+    interfaces,
+    rules
 }
